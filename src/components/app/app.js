ba73982d5adb14f011fbeb405d57c66de2fb07d0
mani <mani.r16@gmail.com>
2019-03-16 14:00:47 +0530

    react boiler plate with hooks

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+/** eslint-disable-next-line */
+    render () {
+        return (
+            <div class="wrapper">
+
+            </div>            
+        )
+    }
+
+}
